7c76387f0772c8acf23a236aba85904e6f491535
Tomomi-K1 <tomomi.music@gmail.com>
2023-10-31 20:03:10 -0700

    fixed podzoo github link

--- src/app/projectData.js
+++ src/app/projectData.js
@@ -2,8 +2,8 @@ const projectData =[
     {
         name: "Podzoo",
         url: "https://podzoo.surge.sh/",
-        github: "https://podzoo.surge.sh/",
-        description: "Podzoo is a podcast discovery web application where user can also leave reviews and save favorite podcasts.",
+        github: "https://github.com/Tomomi-K1/podzoo-fullstack",
+        description: "Podzoo is a podcast discovery web application where user can also leave reviews and save favorite podcasts. (Please wait for it to start up. I'm using free tier on Render.com) ",
         imgUrl: "/podzoo.png",
         imgAlt: "screen shot of project",
         usedTech: ["React", "Express", "Node","Material UI", "PostgreSQL" ]
@@ -12,7 +12,7 @@ const projectData =[
         name: "Paw Match",
         url: "https://find-your-furmily-paw-match.onrender.com/",
         github: "https://github.com/Tomomi-K1/find-your-furmily",
-        description: "Paw Match helps you search adoptable animals. I love animals and I wanted to make something that could help animals get adopted. ",
+        description: "Paw Match helps you search adoptable animals. I love animals and I wanted to make something that could help animals get adopted. (Please wait for it to start up. I'm using free tier on Render.com) ",
         imgUrl: "/paw-match.png",
         imgAlt: "screen shot of project",
         usedTech: ["Flask", "Python", "SQLAlchemy", "PostgreSQL", "Jinja", "Javascript","Bootstrap", "JQuery", "CSS"]
