026f9f1ea43bb698883a323b227e2f9f202d8938
Tomomi <tomomi.n.kosaka@gmail.com>
2025-09-16 21:36:39 -0700

    updated tech skill list

--- src/app/components/TechSkill.jsx
+++ src/app/components/TechSkill.jsx
@@ -1,46 +1,49 @@
 'use client'
-import React, { useEffect } from'react';
+import React, { useEffect } from 'react';
 import Image from 'next/image';
 import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
 import { AiFillHtml5 } from "react-icons/ai";
 
 
-const frontend =[
-    {name:"React",},
-    {name:"Next.js",},
-    {name:"HTML",},
-    {name:"CSS",},
-    {name:"Javascript",},
-    {name:"JQuery",},
-    {name:"Tailwind CSS",},
-    {name:"Bootstrap",},
-    {name:"Material UI",}
+const frontend = [
+    { name: "React", },
+    { name: "Next.js", },
+    { name: "Javascript", },
+    { name: "Typescript", },
+    { name: "Tailwind CSS", },
+    { name: "Radix UI", },
+    { name: "HTML", },
+    { name: "CSS", }
 ];
 
 const backend = [
-    {name:"Node"},
-    {name:"Express"},
-    {name:"Python"},
-    {name:"Flask"},
-    {name:"PostgreSQL"},
-    {name:"SQLAlchemy"}
+    { name: "Node" },
+    { name: "tRPC" },
+    { name: "Express" },
+    { name: "PostgreSQL" },
+    { name: "Prisma" },
+    { name: "Python" },
 ]
 
 const others = [
-    {name:"Git"},
-    {name:"GitHub"},
-    {name:"Jest"},
-    {name:"Jasmine"}
+    { name: "Git" },
+    { name: "GitHub" },
+    { name: "Jest" },
+    { name: "Vitest" },
+    { name: "Docker" },
+    { name: "Zitadel" },
+    { name: "OpenFGA" },
+    { name: "Jira" },
 ]
 
 
-export default function TechSkill(){
-    useEffect( () => {
-        AOS.init();  
+export default function TechSkill() {
+    useEffect(() => {
+        AOS.init();
     }, [])
-    return(
-        <div className='tech_section w-11/12 mx-auto my-24 max-w-screen-lg'> 
+    return (
+        <div className='tech_section w-11/12 mx-auto my-24 max-w-screen-lg'>
             <h2 className='text-center sm:text-left text-3xl sm:text-4xl md:text-5xl font-bold mb-5'>Tech Skills</h2>
             <div>
                 <TechList title={"Frontend"} list={frontend} />
@@ -51,13 +54,13 @@ export default function TechSkill(){
     );
 }
 
-function TechList({title, list}){
-    return(
+function TechList({ title, list }) {
+    return (
         <article data-aos="fade-right" data-aos-duration="1500" className='pb-5'>
             <h5 className='font-bold text-xl'>{title}</h5>
             <ul className='flex flex-wrap'>
-                {list.map((item, idx) =>{
-                    return(
+                {list.map((item, idx) => {
+                    return (
                         <li key={idx} className='m-2 flex justify-start items-center rounded-3xl px-4 py-1 bg-gray-700'>
                             {item.name}
                         </li>
