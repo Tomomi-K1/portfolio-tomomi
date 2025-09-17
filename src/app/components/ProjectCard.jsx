5f174455aaabce9aa3b26f0f2e8620a1d14e6614
Tomomi <tomomi.n.kosaka@gmail.com>
2025-09-16 21:24:39 -0700

    hiding demo button temporarily

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -13,8 +13,6 @@ export default function ProjectCard({ project }) {
     return (
         <article data-aos="fade-up" data-aos-duration="1500" onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)} className="project-card p-5 rounded-lg  hover:bg-gray-600 hover:bg-opacity-20 hover:shadow-sm hover:shadow-white">
             <img src={project.imgUrl} alt={project.imgAlt} />
-            {/* <p>{project.description}</p> */}
-            {/* <h4>Technologies Used</h4> */}
             <div className='relative'>
                 <h3 className={`text-2xl text-left font-bold my-3 ${hover ? "bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text" : ""}`}>{project.name}</h3>
                 <p className='text-left mb-3'>{project.description}</p>
@@ -24,9 +22,9 @@ export default function ProjectCard({ project }) {
                     )}
                 </ul>
                 <div className='mt-2 flex flex-wrap items-start justify-start gap-3'>
-                    <a href={project.url} target="_blank" rel="noopener noreferrer">
+                    {/* <a href={project.url} target="_blank" rel="noopener noreferrer">
                         <MainButton btnName={"Demo"} />
-                    </a>
+                    </a> */}
                     <a href={project.github} target="_blank" rel="noopener noreferrer">
                         <MainButton btnName={"Code"} />
                     </a>
